2e4c13b9de99e81f83ebcca5aae6dfa8a85118dc
LeilanNaeimi <leilan.n@gmail.com>
2023-12-28 03:40:32 +0300

    add sticky nav

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
- 
+window.onscroll = function () {
+  myFunction();
+};
+
+var nav = document.getElementById('nav');
+var sticky = nav.offsetTop;
+
+function myFunction() {
+  if (window.scrollY >= sticky) {
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');
+  }
+}
